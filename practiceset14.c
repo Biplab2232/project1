8a6861f985f127b82a5c151f1eb2acd97c57177f
Biplab2232 <103573780+Biplab2232@users.noreply.github.com>
2022-04-16 11:36:23 +0530

    Add files via upload

---
+++ practiceset14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+float conv(int c);
+int main(){
+    float c;
+    printf("Enter temperature in celsius ");
+    scanf("%f",&c);
+    printf("Fahrenheit equivalent is %f", conv(c));
+    return 0;
+}
+float conv(int c){
+    float f=(1.8*c)+32;
+    return f;
+}
